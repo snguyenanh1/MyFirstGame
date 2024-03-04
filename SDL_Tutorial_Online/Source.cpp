b2fb5106f8eff64ec8cc7bf399dae6e4f4076057
snguyenanh1 <snguyenanh1@gmail.com>
2024-03-04 21:33:24 +0700

    Upload BT_Online

---
+++ SDL_Tutorial_Online/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "gameFunctions.h"
+#include "graphics.h"
+
+int main(int argc, char* argv[]) {
+	Graphics graphics;
+	graphics.init();
+	SDL_Texture* background = graphics.loadTexture("images/bikiniBottom.jpg");
+	graphics.prepareScene(background);
+
+	graphics.presentScene();
+	waitUntilKeyPressed();
+
+	SDL_Texture* spongeBob = graphics.loadTexture("images/Spongebob.png");
+	graphics.renderTexture(spongeBob, 200, 200);
+
+	graphics.presentScene();
+	waitUntilKeyPressed();
+
+	SDL_DestroyTexture(spongeBob);
+	spongeBob = NULL;
+
+	SDL_DestroyTexture(background);
+	background = NULL;
+
+	graphics.quit();
+
+	return 0;
+}
---
+++ SDL_Tutorial_Online/gameFunctions.h
@@ -0,0 +1,24 @@
+#ifndef _FUNCTIONS__H
+#define _FUNCTIONS__H
+
+#include <SDL.h>
+#include <SDL_image.h>
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const char* WINDOW_TITLE = "SDL_Tutorial";
+
+
+void waitUntilKeyPressed() {
+
+    SDL_Event e;
+    while (true) {
+        if (SDL_PollEvent(&e) != 0 && (e.type == SDL_KEYDOWN || e.type == SDL_QUIT))
+            return;
+        SDL_Delay(100);
+    }
+
+}
+
+#endif
+
